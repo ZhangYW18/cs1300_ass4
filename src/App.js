31da6966380f678d348a8ce9b7423535d27c2293
ZhangYW18 <yiweizh18@gmail.com>
2024-04-03 15:12:49 -0400

    set sidebar width as fixed

--- src/App.js
+++ src/App.js
@@ -180,9 +180,9 @@ function App() {
           <div className={"Selectors"}>
             Time:
             <input id="minimum-release-year" type="number" min="1900" max="2024" onChange={handleLowerYear} value={lowerYear}/> to
-            <label for={"minimum-release-year"} className={"HiddenLabel"}>Filters albums released before this year</label>
+            <label htmlFor={"minimum-release-year"} className={"HiddenLabel"}>Filters albums released before this year</label>
             <input id="maximum-release-year" type="number" min="1900" max="2024" onChange={handleUpperYear} value={upperYear}/>
-            <label for={"maximum-release-year"} className={"HiddenLabel"}>Filters albums released after this
+            <label htmlFor={"maximum-release-year"} className={"HiddenLabel"}>Filters albums released after this
               year</label>
             <FilterButton text={"Apply"} onClick={handleYearFilter}/>
           </div>
